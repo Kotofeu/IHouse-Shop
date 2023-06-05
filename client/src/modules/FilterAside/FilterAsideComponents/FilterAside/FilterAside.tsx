46f9694b8b898005c258beaae9c44bfee40f8ba3
Petrov Danil <cras.petrov@yandex.ru>
2023-06-05 03:03:06 +0200

    Выделение компонента FilterAsideAccordions в модуле FilterAside

--- client/src/modules/FilterAside/FilterAsideComponents/FilterAside/FilterAside.tsx
+++ client/src/modules/FilterAside/FilterAsideComponents/FilterAside/FilterAside.tsx
@@ -1,26 +1,16 @@
-import { useState, ChangeEvent, useEffect, useCallback } from 'react'
-import { brandStore, goodStore } from '../../../../store';
-import { observer } from 'mobx-react-lite';
+import { useState, ChangeEvent, useEffect, useCallback, memo} from 'react'
+import { goodStore } from '../../../../store';
 import { IGoodGetParams } from '../../../../store/GoodStore';
 import { FilterAsideFetching } from '../FilterAsideFetching/FilterAsideFetching';
 
-import Accordion from '../../../../components/Accordion/Accordion';
-import { FilterAsideCheckbox } from '../FilterAsideCheckbox/FilterAsideCheckbox';
-import { FilterTitle } from '../FilterTitle/FilterTitle';
+
 import classes from './FilterAside.module.scss'
 import Loader from '../../../../UI/Loader/Loader';
 import Title from '../../../../UI/Title/Title';
-interface IUniversalItem {
-    id: number;
-    name: string;
-    image?: string;
-    categoryId?: number;
-}
+import { FilterAsideAccordions } from '../FilterAsideAccordions/FilterAsideAccordions';
 
-export const FilterAside = observer(() => {
-    const [isTypesOpen, setIsTypesOpen] = useState(true);
-    const [isCategoriesOpen, setIsCategoriesOpen] = useState(true);
-    const [isBrandsOpen, setIsBrandsOpen] = useState(true);
+
+export const FilterAside = memo(() => {
     const [fields, setFields] = useState<IGoodGetParams>({});
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState<string>('');
@@ -41,61 +31,6 @@ export const FilterAside = observer(() => {
         },
         []
     );
-
-    const accordions = [
-        {
-            name: "Бренд",
-            isOpen: isBrandsOpen,
-            setIsOpen: setIsBrandsOpen,
-            items: brandStore.brands?.rows || [],
-            renderItems: (item: IUniversalItem) => (
-                <FilterAsideCheckbox
-                    key={item.id}
-                    name="brandId"
-                    value={item.id}
-                    isChecked={item.id === fields.brandId}
-                    onChange={onChangeSelectorHandler}
-                    title={item.name}
-                    className={classes.filterAside_checkbox}
-                />
-            )
-        },
-        {
-            name: "Категория товара",
-            isOpen: isCategoriesOpen,
-            setIsOpen: setIsCategoriesOpen,
-            items: goodStore.categories?.rows || [],
-            renderItems: (item: IUniversalItem) => (
-                <FilterAsideCheckbox
-                    key={item.id}
-                    name="categoryId"
-                    value={item.id}
-                    isChecked={item.id === fields.categoryId}
-                    onChange={onChangeSelectorHandler}
-                    title={item.name}
-                    className={classes.filterAside_checkbox}
-                />
-            )
-        },
-        {
-            name: "Тип товара",
-            isOpen: isTypesOpen,
-            setIsOpen: setIsTypesOpen,
-            items: goodStore.types?.rows || [],
-            renderItems: (item: IUniversalItem) => (
-                <FilterAsideCheckbox
-                    key={item.id}
-                    name="typeId"
-                    value={item.id}
-                    isChecked={item.id === fields.typeId}
-                    onChange={onChangeSelectorHandler}
-                    title={item.name}
-                    className={classes.filterAside_checkbox}
-                />
-            )
-        }
-    ];
-
     return (
         <aside className={classes.filterAside}>
             <FilterAsideFetching
@@ -107,32 +42,17 @@ export const FilterAside = observer(() => {
                 isLoading && <Loader />
             }
             {
-                !error && !isLoading && accordions.length
-                    ? accordions.map(({ name, isOpen, setIsOpen, items, renderItems }) => (
-                        items?.length ? (
-                            <Accordion
-                                key={name}
-                                className={classes.filterAside_accordion}
-                                titleElement={
-                                    <FilterTitle
-                                        className={classes.filterAside_title}
-                                        hideFilter={() => setIsOpen(v => !v)}
-                                        title={name}
-                                    />
-                                }
-                                isMouseReact={false}
-                                isOpen={isOpen}
-                                setIsOpen={setIsOpen}
-                                items={items}
-                                renderItem={renderItems}
-                            />
-                        ) : null
-                )) : null
+                !error && !isLoading
+                    ? <FilterAsideAccordions
+                        fields={fields}
+                        onChangeSelectorHandler={onChangeSelectorHandler}
+                    />
+                    : null
             }
             {
                 error
-                ?<Title className={classes.filterAside_error}>{error}</Title>
-                :null
+                    ? <Title className={classes.filterAside_error}>{error}</Title>
+                    : null
             }
         </aside>
     );
---
+++ client/src/modules/FilterAside/FilterAsideComponents/FilterAsideAccordions/FilterAsideAccordions.tsx
@@ -0,0 +1,110 @@
+import { observer } from 'mobx-react-lite';
+import { FC, useState, ChangeEvent } from 'react'
+import { brandStore } from '../../../../store';
+import { FilterAsideCheckbox } from '../FilterAsideCheckbox/FilterAsideCheckbox';
+import { IGoodGetParams, goodStore } from '../../../../store/GoodStore';
+import Accordion from '../../../../components/Accordion/Accordion';
+import { FilterTitle } from '../FilterTitle/FilterTitle';
+
+import classes from './FilterAsideAccordions.module.scss'
+
+interface IFilterAsideAccordions {
+    fields: IGoodGetParams;
+    onChangeSelectorHandler: (event: ChangeEvent<HTMLInputElement>) => void
+}
+
+interface IUniversalItem {
+    id: number;
+    name: string;
+    image?: string;
+    categoryId?: number;
+}
+
+export const FilterAsideAccordions: FC<IFilterAsideAccordions> = observer((props) => {
+    const { fields, onChangeSelectorHandler } = props;
+    const [isTypesOpen, setIsTypesOpen] = useState(true);
+    const [isCategoriesOpen, setIsCategoriesOpen] = useState(true);
+    const [isBrandsOpen, setIsBrandsOpen] = useState(true);
+    const checkboxClassName = classes.filterAside_checkbox;
+    const accordions = [
+        {
+            name: "Бренд",
+            isOpen: isBrandsOpen,
+            setIsOpen: setIsBrandsOpen,
+            items: brandStore.brands?.rows || [],
+            renderItems: (item: IUniversalItem) => (
+                <FilterAsideCheckbox
+                    key={item.id}
+                    name="brandId"
+                    value={item.id}
+                    isChecked={item.id === fields.brandId}
+                    onChange={onChangeSelectorHandler}
+                    title={item.name}
+                    className={checkboxClassName}
+                />
+            )
+        },
+        {
+            name: "Категория товара",
+            isOpen: isCategoriesOpen,
+            setIsOpen: setIsCategoriesOpen,
+            items: goodStore.categories?.rows || [],
+            renderItems: (item: IUniversalItem) => (
+                <FilterAsideCheckbox
+                    key={item.id}
+                    name="categoryId"
+                    value={item.id}
+                    isChecked={item.id === fields.categoryId}
+                    onChange={onChangeSelectorHandler}
+                    title={item.name}
+                    className={checkboxClassName}
+                />
+            )
+        },
+        {
+            name: "Тип товара",
+            isOpen: isTypesOpen,
+            setIsOpen: setIsTypesOpen,
+            items: goodStore.types?.rows || [],
+            renderItems: (item: IUniversalItem) => (
+                <FilterAsideCheckbox
+                    key={item.id}
+                    name="typeId"
+                    value={item.id}
+                    isChecked={item.id === fields.typeId}
+                    onChange={onChangeSelectorHandler}
+                    title={item.name}
+                    className={checkboxClassName}
+                />
+            )
+        }
+    ];
+
+    return (
+        <>
+            {
+                accordions.length ? accordions.map(({ name, isOpen, setIsOpen, items, renderItems }) => (
+                    items?.length ? (
+                        <Accordion
+                            key={name}
+                            className={classes.filterAside_accordion}
+                            titleElement={
+                                <FilterTitle
+                                    className={classes.filterAside_title}
+                                    hideFilter={() => setIsOpen(v => !v)}
+                                    title={name}
+                                />
+                            }
+                            isMouseReact={false}
+                            isOpen={isOpen}
+                            setIsOpen={setIsOpen}
+                            items={items}
+                            renderItem={renderItems}
+                        />
+                    ) : null
+
+                )) : null
+            }
+        </>
+    )
+})
